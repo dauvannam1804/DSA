a4f1e49d6aac6e1d7391a5476786e6cf46beb163
Đậu Văn Nam <dauvannam321@gmail.com>
2024-09-08 23:22:50 +0700

    Update Prim2.py

--- Prim2.py
+++ Prim2.py
@@ -6,7 +6,6 @@ def prims(src, dist, visited, graph):
     pq = queue.PriorityQueue()
     pq.put((0, src))
     dist[src] = 0
-    total_weight = 0
 
     while not pq.empty():
         current_dist, u = pq.get()
@@ -15,28 +14,15 @@ def prims(src, dist, visited, graph):
             continue
         
         visited[u] = True
-        total_weight += current_dist
 
         for v, w in graph[u]:
             if not visited[v] and w < dist[v]:
                 dist[v] = w
-                # path[v] = u
                 pq.put((w, v))
-
-    return total_weight
-
-# def printMST(n, path, dist):
-#     ans = 0
-#     for i in range(n):
-#         if path[i] != -1:
-#             ans += dist[i]
-#             # print("{0} - {1}: {2}".format(path[i], i, dist[i]))
-#     # print("Weight of MST: {0}".format(ans))
 
 n, m = map(int, input().split())
 graph = [[] for _ in range(n)]
 dist = [INF] * n
-# path = [-1] * n
 visited = [False] * n
 
 for _ in range(m):
@@ -46,5 +32,8 @@ for _ in range(m):
     graph[u].append((v, w))
     graph[v].append((u, w))
 
-print(prims(0, dist, visited, graph))
-# print(printMST(n, path, dist))
+prims(0, dist, visited, graph)
+sumMST = 0
+for i in range(n):
+    sumMST += dist[i]
+print(sumMST)
